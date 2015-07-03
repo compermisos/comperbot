2ecfdf8d6c5488b3539afc3fb8d610e07dc94a8b
Jesus Christian Cruz Acono <compermisos@gmail.com>
2015-07-02 23:15:46 -0500

    added more patterns

--- bot-aiml.py
+++ bot-aiml.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
+# encoding=utf8
 import os
 import sys
+reload(sys)  
+sys.setdefaultencoding('utf8')
 sys.path.append(os.path.join(os.path.dirname(__file__), 'pyAIML/'))
 import telebot
 import time
@@ -11,6 +14,7 @@ import pyAIML
 TOKEN = os.environ['TELEGRAM_TOKEN']
 k = pyAIML.Kernel()
 k.learn(os.path.join(os.path.dirname(__file__), 'AIML/comperBot/') + "*.aiml")
+k.setBotPredicate("bot_name", "Belinda")
 
 def listener(*messages):
     f = open('chat.log', 'a')
@@ -23,9 +27,10 @@ def listener(*messages):
         chatid = m.chat.id
         if m.content_type == 'text':
             text = m.text
+            response = k.respond(text)
             tb.send_message(chatid, "Escribiste: " + text)
-            tb.send_message(chatid, k.respond(text))
-            f.write(str(datetime.now()) + "\|Chat: " + str(chatid) + "\|Msg: " +  text + "\|\n")
+            tb.send_message(chatid, response)
+            f.write(str(datetime.now()) + "\t Chat: " + str(chatid) + "\t Msg: " +  text + "\t Respuesta: " + response + "\n")
 
 
 tb = telebot.TeleBot(TOKEN)
