ab74c7534cec697a38a83e34e61b9f2d4d191812
Jesus Christian Cruz Acono <compermisos@gmail.com>
2015-07-03 12:19:02 -0500

    added some changes, added brain creation and recovery, session soupourt, and some tweaks

--- bot-aiml.py
+++ bot-aiml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 # encoding=utf8
 import os
+import os.path
 import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
@@ -9,12 +10,25 @@ import telebot
 import time
 from datetime import datetime 
 import pyAIML
+import atexit
 
 
 TOKEN = os.environ['TELEGRAM_TOKEN']
+AIMLName = "comperBot"
+BootName = "belinda"
+BRAINNAME = "brain.sav"
+
 k = pyAIML.Kernel()
-k.learn(os.path.join(os.path.dirname(__file__), 'AIML/comperBot/') + "*.aiml")
-k.setBotPredicate("bot_name", "Belinda")
+if os.path.isfile(BRAINNAME):
+    k.loadBrain(BRAINNAME)
+k.setTextEncoding("UTF-8")
+k.learn(os.path.join(os.path.dirname(__file__), 'AIML/', AIMLName + "/") + "*.aiml")
+k.loadSubs(os.path.join(os.path.dirname(__file__), 'AIML/', AIMLName + "/") + AIMLName+ ".ini")
+k.setBotPredicate("bot_name", BootName)
+
+
+
+atexit.register(lambda : k.saveBrain(BRAINNAME))
 
 def listener(*messages):
     f = open('chat.log', 'a')
@@ -27,7 +41,7 @@ def listener(*messages):
         chatid = m.chat.id
         if m.content_type == 'text':
             text = m.text
-            response = k.respond(text)
+            response = k.respond(text, chatid)
             tb.send_message(chatid, "Escribiste: " + text)
             tb.send_message(chatid, response)
             f.write(str(datetime.now()) + "\t Chat: " + str(chatid) + "\t Msg: " +  text + "\t Respuesta: " + response + "\n")
